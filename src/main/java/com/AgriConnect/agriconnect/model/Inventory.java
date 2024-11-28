077b2c97c137b983130dcad06ca0b25560be94c0
tushar <tusharkokane366@gmail.com>
2024-11-28 09:23:19 +0530

    "Removed unnecessary whitespace and added whitespace for consistency in Inventory.java"

--- src/main/java/com/AgriConnect/agriconnect/model/Inventory.java
+++ src/main/java/com/AgriConnect/agriconnect/model/Inventory.java
@@ -8,17 +8,17 @@ import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.OneToMany;
 import lombok.Data;
-
+  
 @Entity
 @Data
-public class Inventory {
+public class Inventory { 
     
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     private Integer stockLevel;
-
-    @OneToMany
+ 
+    @OneToMany 
     private List<Product> product;  
 
 }
